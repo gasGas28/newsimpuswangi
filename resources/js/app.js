c28b40ec1a4011f79b93ed7fb36d76d0ac994957
Ahmad Nur Faizin <faizintenan@Gmail.com>
2025-10-11 22:24:20 +0700

    fix LOGIN

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
 import 'tailwindcss';
+import axios from 'axios';
+
+// Arahkan axios ke backend Laravel
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
+// Kirim cookie session/XSRF
+axios.defaults.withCredentials = true;
+
+// Header standar AJAX
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// (Jika TIDAK pakai Sanctum) ambil token dari <meta name="csrf-token">
+const token = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+if (token) {
+  axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+}
+
+// (Opsional) export ke window biar gampang dipakai di komponen
+window.axios = axios;
 
 createInertiaApp({
   // title: (title) => `${title} - ${appName}`,
